048d472d89bfbf4ff5ae07311799ae0431837b70
Kyle Pfromer <kpfromer2@gmail.com>
2017-08-12 10:42:56 -0600

    New buttons

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -10,4 +10,14 @@ export class EntryComponent {
     title: string = 'My First Photo';
     photo: string = 'http://placehold.it/800x500?text=Angular Basics';
     description: string = 'A description of angular basics course';
+    comments: any[] = [
+        {
+            name: 'John',
+            comment: 'I liked the video.'
+        },
+        {
+            name: 'Jack',
+            comment: 'I disliked the video.'
+        }
+    ]
 }
