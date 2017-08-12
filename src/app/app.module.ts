e3f46ff2c6830529be895ecb5a84025051f7f519
Kyle Pfromer <kpfromer2@gmail.com>
2017-08-12 10:43:42 -0600

    Imported http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpModule} from "@angular/http";
 
 // Components
 import { AppComponent } from "./app.component"
@@ -11,7 +12,10 @@ Angular compiler uses these decorator to include information
 and do other things that may be repetitive without it
 */
 @NgModule({
-    imports: [BrowserModule],// angular now knows that this app will be used with the web browser
+    imports: [
+        BrowserModule,
+        HttpModule
+    ],// angular now knows that this app will be used with the web browser
     declarations: [
         AppComponent,
         EntryComponent,// Since EntryComponent is a child of the EntryListComponent WE PLACE IT BEFORE EntryListComponent in declarations, else we get an error
--- src/app/entries/entry-list/entry-list.component.ts
+++ src/app/entries/entry-list/entry-list.component.ts
@@ -1,5 +1,5 @@
 import {Component} from "@angular/core";
-
+import {Http} from "@angular/http";
 
 @Component({
     selector: 'app-entry-list',
@@ -7,5 +7,9 @@ import {Component} from "@angular/core";
     styleUrls: ['entry-list.component.css']
 })
 export class EntryListComponent {
-
+    constructor(http: Http){//Dependency inject, angular handles this, similar to spring @autowired
+        http.get('/app/entries/').toPromise()
+            .then(response => {debugger})
+            .catch(error => {debugger});
+    }
 }
